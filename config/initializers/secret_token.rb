1cbfc9a1babe3856f4faea1726141f5bb24e16ec
Hamed Ramezanian <iceage2098@gmail.com>
2012-06-16 20:33:10 +0430

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nerdnews::Application.config.secret_token = '0ac460eed90b02b69331bd09c5ed0fa62c9103ebdabe046430759beba5277bdc9db43e97cdd439ad570b2eab6e637c8e5be3a25619549aef6263b8ecff75c140'
