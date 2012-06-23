0ff890bce8ee7d8ad4fdb69317579c5599a5d399
Hamed Ramezanian <iceage2098@gmail.com>
2012-06-23 12:59:27 +0430

    add some validations on story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,6 @@
 class Story < ActiveRecord::Base
-  attr_accessible :content, :excerpt, :publish_date, :slug, :title, :user_id
+  attr_accessible :content, :excerpt, :publish_date, :title
+  validates_length_of :title, maximum: 100, minimum: 10
+  validates_length_of :content, minimum: 20, maximum: 1000
+  validates  :title, :content, :excerpt, presence: true
 end
