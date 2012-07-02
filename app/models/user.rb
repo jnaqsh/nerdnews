b2dd30a3654735193da92a3bff88bee68190516c
Hamed Ramezanian <iceage2098@gmail.com>
2012-07-02 13:30:37 +0430

    Develop: add some validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  attr_accessible :email, :full_name, :password
+  attr_accessible :email, :full_name, :password, :password_confirmation
+
+  validates_presence_of :full_name
+  validates :password, confirmation: true, presence: true, on: :create
+  validates :email, email_format: true
+  validates_length_of :full_name, maximum: 30, minimum: 7
 end
