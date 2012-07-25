873215506624849cdf7fa2c1d8504f39739edb84
Hamed Ramezanian <iceage2098@gmail.com>
2012-07-25 17:55:06 +0430

    Update: add translation for email format validator error message

--- lib/email_format_validator.rb
+++ lib/email_format_validator.rb
@@ -1,7 +1,7 @@
 class EmailFormatValidator < ActiveModel::EachValidator
   def validate_each(object, attribute, value)
     unless value =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-      object.errors[attribute] << (options[:message] || "is not formatted properly")
+      object.errors[attribute] << (options[:message] || I18n.t('errors.messages.email_format'))
     end
   end
 end 
