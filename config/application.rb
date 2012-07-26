00ac69528a25ddd730b524c9a27a4d953d17e0c7
Hamed Ramezanian <iceage2098@gmail.com>
2012-07-27 00:08:57 +0430

    Fix: initialize on precompile for compiling assets on the development machine

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Nerdnews
 
     # Custom directories with classes and modules you want to be autoloadable.
      config.autoload_paths += %W(#{config.root}/lib)
-
+    config.assets.initialize_on_precompile = false
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
