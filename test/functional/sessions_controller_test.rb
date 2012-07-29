a859837f6071f4b4ca0e0a064b8cba5b57150e43
Arash Mousavi <mousavi.arash@gmail.com>
2012-07-29 22:34:50 +0430

    Add: Some test for login and logout in session

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -6,14 +6,21 @@ class SessionsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "should login" do
+    dave = users(:one)
+    post :create, email: dave.email, password: 'secret'
+    assert_redirected_to root_url
+    assert_equal dave.id, session[:user_id]
   end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
+  test "should fail login" do
+    dave = users(:one)
+    post :create, email: dave.email, password: 'wrong'
+    assert_redirected_to new_session_url
   end
 
+  test "should logout" do
+    delete :destroy
+    assert_redirected_to root_url
+  end
 end
