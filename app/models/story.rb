434b335ecb2431778c0ee68aecb43266af500e6d
Arash Mousavi <mousavi.arash@gmail.com>
2012-08-01 18:32:19 +0430

    Fix: more efficient way of viewing tags by counter in db

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,7 +3,9 @@ class Story < ActiveRecord::Base
 
   has_many :comments, dependent: :destroy
   has_many :taggings, dependent: :destroy
-  has_many :tags, :through => :taggings
+  has_many :tags, :through => :taggings,
+                  :after_add => :calculate_count_and_percentage,
+                  :after_remove => :calculate_count_and_percentage
   belongs_to :user
 
   validates_length_of :title, maximum: 100, minimum: 10
@@ -11,7 +13,7 @@ class Story < ActiveRecord::Base
   validates  :title, :content, presence: true
 
   attr_reader :tag_names
-
+  
   def tag_names=(tokens)
     self.tag_ids = Tag.ids_from_tokens(tokens)
   end
@@ -19,4 +21,10 @@ class Story < ActiveRecord::Base
   def comments_count
     self.comments.count
   end
+  private
+
+    def calculate_count_and_percentage(tag)
+      tag.update_attribute :stories_count, tag.stories.count
+      tag.update_attribute :stories_percentage, (tag.stories.count.to_f / Tag.sum(:stories_count).to_f * 100).floor
+    end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,19 +5,6 @@ class Tag < ActiveRecord::Base
 
   has_many :taggings, dependent: :destroy
   has_many :stories, :through => :taggings
-
- def count_all_tags
-    Tagging.all.count
-  end
-
-  def count_specific_tag(tag)
-    Tag.find(tag).stories.count
-  end
-
-  def percentage_of_tag
-    percent = self.count_specific_tag(self.id).to_f / count_all_tags.to_f * 100.0
-    percent.floor + 100
-  end
     
   private
 
