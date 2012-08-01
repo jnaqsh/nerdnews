981507d37b7907113b43c6d8398c10781f9aea87
Arash Mousavi <mousavi.arash@gmail.com>
2012-08-01 19:05:10 +0430

    Add: comments count to stories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120731200520) do
+ActiveRecord::Schema.define(:version => 20120801141549) do
 
   create_table "comments", :force => true do |t|
     t.string   "name"
@@ -48,8 +48,9 @@ ActiveRecord::Schema.define(:version => 20120731200520) do
     t.datetime "publish_date"
     t.integer  "user_id"
     t.string   "slug"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.integer  "comments_count", :default => 0
   end
 
   create_table "taggings", :force => true do |t|
