542c964cb5319c688573c0f3e6469e1a44d41562
Arash Mousavi <mousavi.arash@gmail.com>
2012-09-23 12:53:38 +0330

    Add: pagination for users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.order('created_at desc').page params[:page]
 
     respond_to do |format|
       format.html # index.html.erb
