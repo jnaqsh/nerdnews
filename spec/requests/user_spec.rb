00dec5c70b3c62825001a329273116bfe17ab42d
Arash Mousavi <mousavi.arash@gmail.com>
2012-10-03 10:52:20 +0330

    Add test for js rating button for stories

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -3,20 +3,31 @@ require 'spec_helper'
 
 describe "Users" do
   context 'Rating' do
+    
+    before do
+      @user = FactoryGirl.create(:user)
+      login @user
+      @story = FactoryGirl.create(:story, user: @user)
+      visit story_path @story
+    end
+
     it 'should add a point after commenting/replaying' do
-      user = FactoryGirl.create(:user)
-      login user
-      story = FactoryGirl.create(:story, user: user)
-      visit story_path story
       expect {
         fill_in 'دیدگاه', with: 'comment'
         click_button 'ایجاد'
-      }.to change { user.reload.user_rate }.by(1)
+      }.to change { @user.reload.user_rate }.by(1)
       expect {
         click_on 'پاسخ'
         fill_in 'دیدگاه', with: 'comment'
         click_button 'ایجاد'
-      }.to change { user.reload.user_rate }.by(1)
+      }.to change { @user.reload.user_rate }.by(1)
+    end
+
+    it 'should show the rating items for story', js: true do
+      click_button 'btn-thumbs-up'
+      page.should have_content 'جالب'
+      click_button 'btn-thumbs-down'
+      page.should have_content 'قدیمی'
     end
   end
 end
