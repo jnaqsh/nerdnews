8d431d270840a14f28f05e56c88b2892bdf576b7
Arash Mousavi <mousavi.arash@gmail.com>
2012-10-20 13:33:29 +0330

    add test for filtering tags

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -6,6 +6,7 @@ describe "Tags" do
     admin = FactoryGirl.create(:admin_user)
     login admin
   end
+  
   it "should make a new tag" do
     visit tags_url
     click_on "جدید"
@@ -13,13 +14,25 @@ describe "Tags" do
     fill_in 'نام', with: "tag"
     click_button "ایجاد"
   end
+  
   it "should edit a tag" do
     tag = FactoryGirl.create(:tag)
-    visit tags_url
+    visit tags_path
     click_on "ویرایش"
     fill_in 'نام', with: 'edited'
     click_button 'تگ'
     tag.reload.name.should == 'edited'
     page.should have_content 'موفقیت'
+  end
+
+  it 'filters stories by tag' do
+    story = FactoryGirl.build(:approved_story)
+    tag = FactoryGirl.create(:tag)
+    story.tags << tag
+    story1 = FactoryGirl.create(:approved_story)
+    visit tags_path
+    click_link tag.name
+    page.should have_content(story.title)
+    page.should_not have_content(story1.title)
   end
 end
