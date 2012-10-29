515df86d8d05f306e4091664cb495fed2bb38647
Arash Mousavi <mousavi.arash@gmail.com>
2012-10-29 14:18:23 +0330

    Fix mypage for favorite tags array

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -2,12 +2,12 @@ class MypageController < ApplicationController
   authorize_resource :class => false
   def index
 
-    @favorite_tags = current_user.favorite_tags
+    @favorite_tags = current_user.favorite_tags_array
     
     if @favorite_tags.blank?
       flash[:notice] = t('.controllers.mypage.flash.add_tags')
     else
-      @stories = Story.joins(:tags).where('tags.name' => @favorite_tags.split(','))
+      @stories = Story.joins(:tags).where('tags.name' => @favorite_tags).order('created_at desc').page params[:page]
       flash[:error] = t('.controllers.mypage.flash.nothing_found') if @stories.blank?
     end
   end
