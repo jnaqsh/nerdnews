dc8c72f3352914915c4bf397fe700024a3a8d426
Hamed Ramezanian <iceage2098@gmail.com>
2012-10-31 13:53:18 +0330

    adds some data like roles and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+#encoding:utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -7,13 +9,37 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.delete_all
-admin = User.create!(
-                    :full_name              => "adminadmin", 
-                    :email                  => "admin@admin.com", 
-                    :password               => "admin", 
-                    :password_confirmation  => "admin"
+founder1 = User.create!(
+                    :full_name              => "حامد رمضانیان",
+                    :email                  => "h.ramezanian@jnaqsh.com",
+                    :password               => "secret",
+                    :password_confirmation  => "secret"
                     )
-Role.delete_all
-role = Role.create([{:name => "admin"}, {:name => "manager"}, {:name => "user"}])
+founder2 = User.create!(
+                    :full_name              => "آرش موسوی",
+                    :email                  => "a.mosavi@jnaqsh.com",
+                    :password               => "secret",
+                    :password_confirmation  => "secret"
+                    )
 
-admin.roles << Role.find_by_name("admin")
+approved = User.create!(
+                    :full_name              => "کاربر تاییدشده",
+                    :email                  => "approved@jnaqsh.com",
+                    :password               => "secret",
+                    :password_confirmation  => "secret"
+                    )
+
+new_user = User.create!(
+                    :full_name              => "کاربر جدید",
+                    :email                  => "new_user@jnaqsh.com",
+                    :password               => "secret",
+                    :password_confirmation  => "secret"
+                    )
+
+Role.delete_all
+Role.create([{:name => "founder"}, {:name => "approved"}, {:name => "new_user"}])
+
+founder1.roles << Role.find_by_name("founder")
+founder2.roles << Role.find_by_name("founder")
+approved.roles << Role.find_by_name("approved")
+new_user.roles << Role.find_by_name("new_user")
