0a30f40f10b994b37234148761119470deef2426
Arash Mousavi <mousavi.arash@gmail.com>
2012-11-20 11:54:14 +0330

    Add validation of weight to ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,6 +3,8 @@ class Rating < ActiveRecord::Base
 
   has_many :votes
 
+  validates_numericality_of :weight, greater_than: -6, less_than: 6
+
   scope :positive, where('weight > 0').order('weight DESC')
   scope :negative, where('weight < 0').order('weight DESC')
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -5,4 +5,26 @@ describe Rating do
   it { should respond_to :name }
   it { should respond_to :weight }
   it { should have_many :votes }
+
+  describe "validations", focus: true do
+    it 'has a valid factory' do
+      FactoryGirl.build(:rating).should be_valid
+    end
+
+    it { should validate_numericality_of :weight }
+    
+    it 'wont accept weights not in the correct range' do
+      wrong_weights = [-7, -6, 6, 7]
+      wrong_weights.each do |weight|
+        FactoryGirl.build(:rating, weight: weight).should_not be_valid
+      end
+    end
+    
+    it 'accepts weights in the correct range' do
+      correct_weight = -5..5
+      correct_weight.each do |weight|
+        FactoryGirl.build(:rating, weight: weight).should be_valid
+      end
+    end
+  end
 end
