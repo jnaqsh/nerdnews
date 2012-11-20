6e15d879e8a75b069d1223ceaebcb7aa6311f577
Arash Mousavi <mousavi.arash@gmail.com>
2012-11-20 11:34:23 +0330

    Add destroy action to rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -8,12 +8,9 @@ class RatingsController < ApplicationController
   end
 
   def new
-    @rating = Rating.new
   end
 
   def create
-    @rating = Rating.new(params[:rating])
-
     respond_to do |format|
       if @rating.save
         format.html { redirect_to ratings_path, notice: t('controllers.ratings.create.flash.success') }
@@ -22,14 +19,20 @@ class RatingsController < ApplicationController
   end
 
   def edit
-    @rating = Rating.find(params[:id])
   end
 
   def update
-    @rating = Rating.find(params[:id])
     respond_to do |format|
       if @rating.update_attributes(params[:rating])
         format.html { redirect_to ratings_path, notice: t('controllers.ratings.update.flash.success') }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @rating.destroy
+        format.html { redirect_to ratings_path, notice: t('controllers.ratings.destroy.flash.success') }
       end
     end
   end
--- spec/requests/ratings_spec.rb
+++ spec/requests/ratings_spec.rb
@@ -3,9 +3,11 @@ require 'spec_helper'
 
 describe "Ratings" do
   describe "GET /ratings" do
-    it 'should add/edit a new rating' do
+    before do
       user = FactoryGirl.create(:founder_user)
       login user
+    end
+    it 'should add/edit a new rating' do
       visit ratings_path
       click_link 'جدید'
       current_path.should eq new_rating_path
@@ -14,11 +16,26 @@ describe "Ratings" do
       click_button 'ایجاد'
       current_path.should eq ratings_path
       page.should have_content 'موفقیت'
-      click_link 'جالب'
+      page.should have_content 'جالب'
+    end
+
+    it 'should edit a rating' do
+      rating = FactoryGirl.create(:rating)
+      visit ratings_path
+      click_link rating.name
       fill_in 'نام', with: 'قشنگ'
       click_button 'به‌روزرسانی'
       current_path.should eq ratings_path
       page.should have_content 'موفقیت'
+      page.should have_content 'قشنگ'
+    end
+
+    it 'should destroy a rating' do
+      rating = FactoryGirl.create(:rating)
+      visit ratings_path
+      click_link 'حذف'
+      page.should have_content 'موفقیت'
+      page.should_not have_content rating.name
     end
   end
 end
