1f971d09300bdc5d3467fd77dd027911956c6fbc
Arash Mousavi <mousavi.arash@gmail.com>
2012-12-16 11:16:59 +0330

    remove debug print

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,6 @@ require 'faker'
 describe UsersController do
   it "should creates a user successfully" do
     post :create, user_full_name: "full_name", user_email: "user@example.com"
-    # pp assigns[:user]
+    assigns[:user]
   end
 end
