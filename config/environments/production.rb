7b9a96955b158ca91cf1b80415273b37599bbe72
Hamed Ramezanian <iceage2098@gmail.com>
2013-02-14 12:37:30 +0330

    sets default url for mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,8 @@ Nerdnews::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = {host: "nerdnews.ir"}
+
   config.paperclip_defaults = {
     :storage => :dropbox,
     :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
