d827742019abecf56bced99d7b5f6e21cec6a71b
Hamed Ramezanian <iceage2098@gmail.com>
2013-02-16 16:53:37 +0330

    remvoes under construction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # Editing UnderConstruction block causes it's clear generator to not work
-  before_filter :redirect_to_under_construction
-
-  # Will redirect all requests to under construction page
-  def redirect_to_under_construction
-    if request.host_with_port == UnderConstruction.config.host_name && Rails.env.production?
-      unless request.url =~ /(under_construction|email_storage)/
-        redirect_to under_construction_index_path
-      end
-    end
-  end
-
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
