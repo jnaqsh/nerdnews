789690b041ca4c9bdcfec45ce6f78be3c4a591ad
Arash Mousavi <mousavi.arash@gmail.com>
2013-05-19 20:34:02 +0430

    validate uniquness of votes, bug #40

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,10 +4,10 @@ class Vote < ActiveRecord::Base
   belongs_to :rating
 
   # validates_presence_of :story, :rating, :user
-  # validates_uniqueness_of :story_id, scope: [:rating_id, :user_id]
+  validates_uniqueness_of :voteable_id, :scope => :user_id
 
   # Not used anywhere, but who cares? It may come handy later
-  def self.user_voted?(user, story)
-    !Vote.find_by_user_id_and_story_id(user, story).nil?
-  end
+  # def self.user_voted?(user, story)
+  #   !Vote.find_by_user_id_and_story_id(user, story).nil?
+  # end
 end
