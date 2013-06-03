d69d50682337f3f0396b24819ccb40c1ba842ddf
Hamed Ramezanian <iceage2098@gmail.com>
2013-06-03 18:59:06 +0430

    hides secret_token in app_config file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Nerdnews::Application.config.secret_token = '0ac460eed90b02b69331bd09c5ed0fa62c9103ebdabe046430759beba5277bdc9db43e97cdd439ad570b2eab6e637c8e5be3a25619549aef6263b8ecff75c140'
+
+app_config = YAML.load(File.read(Rails.root.join('config', 'application_configs.yml')))
+
+Nerdnews::Application.config.secret_token = app_config["secret_token"]
