d4715a03521088e577ddc24cd31aeef0c61537b5
Arash Mousavi <mousavi.arash@gmail.com>
2013-07-30 15:51:32 +0430

    config mailcatcher in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,5 +41,7 @@ Nerdnews::Application.configure do
     sender_address: 'do_not_reply@nerdnews.ir',
     exception_recipients: "h.ramezanian@jnaqsh.com"
 
-  config.action_mailer.delivery_method = :letter_opener
+  # Configure to use with mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
