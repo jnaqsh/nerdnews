1fb5f612069a6dfe01bee9c11b86e2e8362840b8
Arash Mousavi <mousavi.arash@gmail.com>
2013-07-30 17:26:22 +0430

    Added html temp for comment_reply mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Nerdnews::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = {host: "localhost:3000"}
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
 
   config.middleware.use ExceptionNotifier,
     sender_address: 'do_not_reply@nerdnews.ir',
