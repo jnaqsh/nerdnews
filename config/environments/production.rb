6348998fe3bb1a88750aa93442266147c9fb875d
Arash Mousavi <mousavi.arash@gmail.com>
2013-09-27 14:17:44 +0330

    The Active Record auto explain feature has been removed.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,10 +70,6 @@ Nerdnews::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
   config.action_mailer.default_url_options = {host: "nerdnews.ir"}
 
   config.paperclip_defaults = {
