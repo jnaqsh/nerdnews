3f3258b1d3c1800d546869efc036be7818112b11
Arash Mousavi <mousavi.arash@gmail.com>
2013-10-04 11:15:44 +0330

    add congig.eager_load to environments while rails 4 needs it

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Nerdnews::Application.configure do
+  # Rails 4 needs this option
+  config.eager_load = false
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
