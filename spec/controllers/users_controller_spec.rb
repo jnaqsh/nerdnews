01190114349c1c40a794bdfddcd3d1c699343b3a
Arash Mousavi <mousavi.arash@gmail.com>
2013-10-21 19:13:08 +0330

    Fixes the create controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'faker'
 
 describe UsersController do
   it "should creates a user successfully" do
-    post :create, user_full_name: "full_name", user_email: "user@example.com"
-    assigns[:user]
+    post :create, user: { full_name: "full_name", email: "user@example.com" }
+    assigns(cookies[:user_id])
   end
 end
