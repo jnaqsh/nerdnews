02a0a42af1771984093c4e74d24ca6a05981f80b
Arash Mousavi <mousavi.arash@gmail.com>
2013-10-21 18:29:03 +0330

    Corrects api visit path in test

--- spec/features/api/v1/stories_spec.rb
+++ spec/features/api/v1/stories_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe "Stories API", solr: true, focus: true do
+describe Api::V1::StoriesController, solr: true do
   it 'sends a list of messages' do
     FactoryGirl.create_list(:approved_story, 10)
     Sunspot.commit
 
-    visit '/stories.json'
+    visit '/api/v1/stories.json'
 
     json = JSON.parse(page.body)
     expect(json.length).to eq(10) # check to make sure the right amount of messages are returned
