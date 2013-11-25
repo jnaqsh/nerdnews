29e68f047473620ee88770b4ba6481fae1addb09
Arash Mousavi <mousavi.arash@gmail.com>
2013-11-25 22:01:12 +0330

    Updates twitter configuration file to new syntax

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -2,7 +2,7 @@ require 'twitter'
 
 twitter_config = YAML.load(File.read(Rails.root.join('config', 'twitter.yml')))[Rails.env]
 
-Twitter.configure do |config|
+client = Twitter::REST::Client.new do |config|
   config.consumer_key =twitter_config['consumer_key']
   config.consumer_secret = twitter_config['consumer_secret']
   config.oauth_token = twitter_config['oauth_token']
